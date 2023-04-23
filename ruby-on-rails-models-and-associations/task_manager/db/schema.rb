0cf1916a49596685ffa6294ea3dc3c95565782eb
Gegardus <stgegard@gmail.com>
2023-04-23 20:44:56 +0400

    ActiveRecord Assosiations

--- ruby-on-rails-models-and-associations/task_manager/db/schema.rb
+++ ruby-on-rails-models-and-associations/task_manager/db/schema.rb
@@ -11,28 +11,28 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2022_06_08_172653) do
-  create_table "categories", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+  create_table "categories", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "tags", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+  create_table "tags", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "tags_tasks", id: false, charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
-    t.bigint "tag_id", null: false
-    t.bigint "task_id", null: false
+  create_table "tags_tasks", id: false, force: :cascade do |t|
+    t.integer "tag_id", null: false
+    t.integer "task_id", null: false
     t.index ["tag_id", "task_id"], name: "index_tags_tasks_on_tag_id_and_task_id"
     t.index ["task_id", "tag_id"], name: "index_tags_tasks_on_task_id_and_tag_id"
   end
 
-  create_table "task_assignments", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
-    t.bigint "task_id", null: false
-    t.bigint "user_id", null: false
+  create_table "task_assignments", force: :cascade do |t|
+    t.integer "task_id", null: false
+    t.integer "user_id", null: false
     t.string "role"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -42,7 +42,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_08_172653) do
     t.index ["user_id"], name: "index_task_assignments_on_user_id"
   end
 
-  create_table "tasks", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+  create_table "tasks", force: :cascade do |t|
     t.string "name"
     t.text "description"
     t.integer "position"
@@ -52,7 +52,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_08_172653) do
     t.integer "category_id"
   end
 
-  create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+  create_table "users", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
     t.string "email"
